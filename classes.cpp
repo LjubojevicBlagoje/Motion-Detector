bf9f327e02a2f6e4ea97042569c0c6684fb92f92
LjubojevicBlagoje <ljubojevicblagoje8@gmail.com>
2025-09-26 21:56:00 +0930

    WIP: pixel changes

--- classes.cpp
+++ classes.cpp
@@ -1,9 +1,2 @@
 #include "classes.h"
 
-// Pixel implementation
-Pixel::Pixel() : r(0), g(0), b(0) {};
-Pixel::Pixel(uint8_t r, uint8_t g, uint8_t b) {
-  this->r = r;
-  this->g = g;
-  this->b = b;
-}
